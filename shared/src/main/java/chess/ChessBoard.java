390dfbbd43db34a7f2db1b386b0dbac20c8536d4
Garrett Vance <garrettsvance@gmail.com>
2024-02-06 14:38:44 -0700

    minor changes

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -108,7 +108,7 @@ public class ChessBoard {
         for (int i = 1; i <= 8; i++) {
             for (int j = 1; j <= 8; j++) {
                String pieceAbbr = getPieceAbbr(i, j);
-               System.out.print("| " + (board[i][j] != null ? pieceAbbr : " ") + " ");
+               System.out.print("|" + (board[i][j] != null ? pieceAbbr : " "));
             }
             System.out.println("|");
         }
