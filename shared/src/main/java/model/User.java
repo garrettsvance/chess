d4dbeaf9dc2881273dfa65536c089efd5920a1ed
garrettpants <garrettsvance@gmail.com>
2024-02-13 16:04:36 -0700

    added user class to model

---
+++ shared/src/main/java/model/User.java
@@ -0,0 +1,27 @@
+package model;
+
+public class User {
+
+    String userName;
+    String password;
+    String email;
+
+    public User(String userName, String password, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
