228457580d19dc2c5941053545d6927f28110f58
garrettpants <garrettsvance@gmail.com>
2024-02-13 17:44:17 -0700

    created registerservice

---
+++ server/src/main/java/service/RegisterService.java
@@ -0,0 +1,29 @@
+package service;
+import model.AuthData;
+import model.UserData;
+import dataAccess.*;
+
+import java.util.UUID;
+
+public class RegisterService {
+
+    record RegisterRequest(String username, String password, String email) {}
+    record RegisterResult(String username, String authToken, String message) {}
+    static userDAO userDAO = new userDAO();
+    static authTokenDAO authDAO = new authTokenDAO();
+
+    public static RegisterResult register(RegisterRequest request) {
+        if (request.email == null || request.username == null || request.password == null) {
+            return new RegisterResult(null, null, "Error: bad request");
+        }
+        if (userDAO.findUser(request.username) != null) {
+            return new RegisterResult(null, null, "Error: already taken");
+        } else {
+            userDAO.insertUser(new UserData(request.username, request.password, request.email));
+            String userToken = UUID.randomUUID().toString();
+            authDAO.addToken(new AuthData(userToken, request.username));
+            return new RegisterResult(request.username, userToken, "success");
+        }
+    }
+
+}
