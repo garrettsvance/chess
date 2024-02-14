015151c2a2edd3e5c6833e378a5eba2ea21fb043
garrettpants <garrettsvance@gmail.com>
2024-02-13 20:47:21 -0700

    list games handler

--- server/src/main/java/server/Handler/JoinGameHandler.java
+++ server/src/main/java/server/Handler/JoinGameHandler.java
@@ -1,28 +1,28 @@
 package server.Handler;
 
-import service.CreateGameService;
+import service.JoinGameService;
 import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
-public class CreateGameHandler implements Route {
+public class JoinGameHandler implements Route {
     @Override
     public Object handle(Request sparkRequest, Response response) throws Exception {
         Gson gson = new Gson();
-
         try {
-            CreateGameService.CreateGameRequest request = gson.fromJson(sparkRequest.body(), CreateGameService.CreateGameRequest.class);
+            JoinGameService.JoinGameRequest request = gson.fromJson(sparkRequest.body(), JoinGameService.JoinGameRequest.class);
             String authToken = sparkRequest.headers("Authorization");
-            CreateGameService.CreateGameResult result = CreateGameService.createGame(request, authToken);
+            JoinGameService.JoinGameResult result = JoinGameService.joinGame(request, authToken);
             switch (result.message()) {
                 case "success" -> response.status(200);
                 case "Error: unauthorized" -> response.status(401);
                 case "Error: bad request" -> response.status(400);
+                case "Error: already taken" -> response.status(403);
             }
             return gson.toJson(result);
         } catch(Exception e) {
-            CreateGameService.CreateGameResult result = new CreateGameService.CreateGameResult(null, "Error:" + e.getMessage());
+            JoinGameService.JoinGameResult result = new JoinGameService.JoinGameResult(null, null, "Error:" + e.getMessage());
             response.status(500);
             return gson.toJson(result);
         }
