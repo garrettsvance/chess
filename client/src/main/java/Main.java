eb35ad11a848693e2a8bb1422eff05fc385eaa81
Garrett Vance <garrettsvance@gmail.com>
2024-03-14 14:35:06 -0600

    added a few more files and dependencies

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,6 +1,10 @@
 import chess.*;
 
-public class Main { // this is the class that you run to start your server and such
+
+// this is the class that you run to start your server and such
+// you'll create separate classes for your chessboard ui and menu ui
+
+public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
