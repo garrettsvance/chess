c48f324ac7be7c36afe38e70c0117964d36373de
garrettpants <garrettsvance@gmail.com>
2024-03-15 14:35:46 -0600

    saving changes to getPieceUI

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -77,5 +77,56 @@ public class ChessBoardUI {
 
     }
 
+    public ChessPiece getPieceUI(ChessPosition position, ChessBoard board) {
+        return board.getPiece(position);
+    }
+
+    /*    private ChessPiece[][] copy() {
+        ChessPiece[][] copiedBoard = new ChessPiece[9][9];
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                if (board[i][j] != null) {
+                    copiedBoard[i][j] = board[i][j].copy();
+                }
+            }
+        }
+        return copiedBoard;
+    }
+       public ChessPiece getPiece(ChessPosition position) {
+        int row = position.getRow();
+        int col = position.getColumn();
+        if (board[row][col] == null) {
+            return null;
+        } else {
+            return board[row][col];
+        }
+    }
+
+        public String getPieceAbbr(int i, int j) {
+        if (board[i][j] != null) {
+            if (board[i][j].getTeamColor() == ChessGame.TeamColor.WHITE) {
+                return switch (board[i][j].getPieceType()) {
+                    case KING -> "K";
+                    case QUEEN -> "Q";
+                    case BISHOP -> "B";
+                    case KNIGHT -> "N";
+                    case ROOK -> "R";
+                    case PAWN -> "P";
+                };
+            } else {
+                return switch (board[i][j].getPieceType()) {
+                    case KING -> "k";
+                    case QUEEN -> "q";
+                    case BISHOP -> "b";
+                    case KNIGHT -> "n";
+                    case ROOK -> "r";
+                    case PAWN -> "p";
+                };
+            }
+        }
+        return null;
+    }
+
+    */
 
 }
