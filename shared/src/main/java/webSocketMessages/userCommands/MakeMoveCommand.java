bcc9d912fca8a5f584c98437f565ad5f877fe72a
garrettpants <garrettsvance@gmail.com>
2024-04-08 13:17:03 -0600

    added makemovecommand

---
+++ shared/src/main/java/webSocketMessages/userCommands/MakeMoveCommand.java
@@ -0,0 +1,29 @@
+package webSocketMessages.userCommands;
+
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand {
+
+    private final int gameID;
+    private final ChessMove move;
+
+    public MakeMoveCommand(String authToken, int gameID, ChessMove move) {
+        super(authToken);
+        this.gameID = gameID;
+        this.move = move;
+        commandType = CommandType.MAKE_MOVE;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public String getAuthToken() {
+        return getAuthTokenString();
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+
+}
